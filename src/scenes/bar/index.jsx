9458b7c173fa67c9dc7d52ff5cafe6a11b149867
dabere <daberechukwu18@gmail.com>
2023-02-08 02:33:05 +0100

    second commit

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -5,12 +5,13 @@ import Header from "../../components/Header";
 
 
 
+
 const Bar = () => {
   return (
     <Box m="20px">
         <Header title="Bar Chart" subtitle="Simple Barchart Chart"/>
         <Box height="75vh">
-           kfeos[dkkfdop[k]]
+           
 
         </Box>
     </Box>
